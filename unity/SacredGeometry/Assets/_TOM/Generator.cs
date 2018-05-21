739c92fa9e3d91d4af90c69dfa0d65e09244a8b6
Unknown <tomas.ruiz.penin@gmail.com>
2018-05-21 15:41:46 +0100

    version 2

--- unity/SacredGeometry/Assets/_TOM/Generator.cs
+++ unity/SacredGeometry/Assets/_TOM/Generator.cs
@@ -5,55 +5,56 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour {
 
+    public float angle_incr;
+
+    public float scaleFactor;
+
+
     public GameObject atomPrefab;
 
     public List<GameObject> atomList;
 
     public int numberOfAtoms = 100;
 
-    public float scaleFactor = 100;
-
-    public float speed=120; 
 
 
-    public float angle_incr;
+    public float initialScale = 1.6f;
+    public float scaleInterval = 1f;  // Amount to move left and right from the start point
+    public float scaleSpeed = 2.0f;
 
-    public float ratio;
+
+    public float initialAngleIncr = 1.6f;
+    public float oscilationInterval = 1f;  // Amount to move left and right from the start point
+    public float oscilationSpeed = 2.0f;
 
     // Use this for initialization
     void Start () {
-
-        angle_incr = 2 * Mathf.PI / numberOfAtoms;
 
         for (int i = 1; i <= numberOfAtoms; ++i)
         {
             GameObject myAtom = Instantiate(atomPrefab);
             atomList.Add(myAtom);
-
-            ratio = i / (float)numberOfAtoms;
-            float spiral_rad = ratio * scaleFactor;
-            float angle = i * angle_incr;
-            float x = transform.position.x + Mathf.Cos(angle) * spiral_rad;
-            float y = transform.position.y + Mathf.Sin(angle) * spiral_rad;
-
-            myAtom.transform.position = new Vector3(x, y, transform.position.z);
             myAtom.transform.SetParent(transform);
-            // draw tiny circle at x,y
-            //ellipse(x, y, sm_diameter, sm_diameter);
+
         }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        //float elapsedSeconds = millis() * .001;
-        angle_incr = Mathf.Deg2Rad * (2f + Time.frameCount / speed);
 
-        //float cx = 100 / 2;
-        //float cy = 100 / 2;
 
-        //float sm_diameter = 4;
+    // Update is called once per frame
+    void Update () {
+
+
+
+   
+        angle_incr = initialAngleIncr;
+        angle_incr += oscilationInterval * Mathf.Sin(Time.time * oscilationSpeed);
+
+        scaleFactor = initialScale;
+        scaleFactor += scaleInterval * Mathf.Sin(Time.time * scaleSpeed);
+
+
 
         for (int i = 1; i <= atomList.Count; ++i)
         {
@@ -62,13 +63,15 @@ public class Generator : MonoBehaviour {
             float ratio = i / (float)numberOfAtoms;
             float spiral_rad = ratio * scaleFactor;
             float angle = i * angle_incr;
-            float x = transform.position.x + Mathf.Cos(angle) * spiral_rad;
-            float y = transform.position.y + Mathf.Sin(angle) * spiral_rad;
+            float x =  + Mathf.Cos(angle) * spiral_rad;
+            float y =  + Mathf.Sin(angle) * spiral_rad;
 
-            atomList[i-1].transform.position = new Vector3(x, y, transform.position.z);
+            float z =  + Mathf.Atan(angle) * spiral_rad;
 
-            // draw tiny circle at x,y
-            //ellipse(x, y, sm_diameter, sm_diameter);
+
+            atomList[i-1].transform.localPosition = new Vector3(x, y, z);
+
+
         }
 
     }
